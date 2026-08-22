e64d82f8e729097a421f77fd0e2730c2c6f5969b
Ulyara <ulyarautama@gmail.com>
2026-08-22 14:11:56 +0700

    initial commit

--- client_web/next.config.ts
+++ client_web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const backendUrl = process.env.BACKEND_URL ?? "http://127.0.0.1:8000";
+
 const nextConfig: NextConfig = {
   reactCompiler: true,
+
   images: {
     remotePatterns: [
       {
@@ -9,6 +12,15 @@ const nextConfig: NextConfig = {
         hostname: "ik.imagekit.io",
       },
     ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: `${backendUrl}/:path*`,
+      },
+    ];
   },
 };
 
